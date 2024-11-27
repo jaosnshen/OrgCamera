bdd101d7c519f94321a00811f2d4f96dfaf1545b
shen <22>
2024-11-27 11:06:27 +0800

    Initial commit

---
+++ RameraLib/src/main/java/com/rb/camera/EasyToast.kt
@@ -0,0 +1,16 @@
+package com.rb.camera
+
+import android.util.Log
+
+class EasyToast {
+
+    companion object{
+        private  val TAG = "EasyToast"
+        fun show(){
+            Log.e(TAG,"show");
+        }
+
+    }
+
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OrgCamera"
+include(":app")
+include(":RameraLib")
